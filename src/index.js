88f6677ff489c5c3fefecd5426763109ffdecf04
Denis <ya.denis300000@yandex.ru>
2023-01-21 13:53:59 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let arr = [];
+	for(let char of str){
+		for(let i = 0; i < bracketsConfig.length; i++){
+			if(arr[arr.length-1] == bracketsConfig[i][0] && char==bracketsConfig[i][1]){
+				arr.pop();
+			}
+			else if (char == bracketsConfig[i][0]){
+				arr.push(char);
+			}
+			else if(char == bracketsConfig[i][1]){
+				return false;
+			}
+		}
+	}
+	return (!arr.length);
 }
